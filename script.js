825eb738b7056bf0c00386700a14b313afb65173
fn26O2 <alvaro.tebarfolch@gmail.com>
2024-11-19 12:44:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// Datos de ejemplo
-var fechas = ['2021-01-01', '2021-02-01', '2021-03-01', '2021-04-01'];
-var precios = [29000, 33000, 45000, 60000];
-
-// Gráfico de líneas
-var dataLine = [{
-    x: fechas,
-    y: precios,
-    type: 'scatter'
-}];
-
-Plotly.newPlot('bitcoin-chart', dataLine);
-
-// Gráfico de barras
-var dataBar = [{
-    x: fechas,
-    y: precios,
-    type: 'bar'
-}];
-
-Plotly.newPlot('bitcoin-bar-chart', dataBar);
+document.addEventListener('DOMContentLoaded', function() {
+    var data = [{
+        x: ['2021-01-01', '2021-02-01', '2021-03-01', '2021-04-01'],
+        y: [29000, 33000, 45000, 60000],
+        type: 'scatter'
+    }];
+    
+    var layout = {
+        title: 'Precio de Bitcoin en 2021',
+        xaxis: {
+            title: 'Fecha'
+        },
+        yaxis: {
+            title: 'Precio en USD'
+        }
+    };
+    
+    Plotly.newPlot('chart1', data, layout);
+});
